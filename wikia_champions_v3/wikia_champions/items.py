7b647ab9e54bc2276f134eaef75f957e061e14e4
huzhx <huzhaoxian89@163.com>
2014-03-23 15:35:02 -0400

    3rd commit - rm script, style, and form (see champsdocs_v3)

---
+++ wikia_champions_v3/wikia_champions/items.py
@@ -0,0 +1,33 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+from scrapy.item import Item, Field
+
+class WikiaChampionsItem(Item):
+    # define the fields for your item here like:
+    # name = Field()
+    pass
+
+class URLItems(Item):
+    url = Field()
+
+class WikiaChampionsList(Item):
+    champ_url = Field()
+    
+class WikiaChampionsHomepage(Item):
+    url = Field()
+    urldoc = Field()
+    
+class WikiaChampionsBackground(Item):
+    url = Field()
+    urldoc = Field()
+
+class WikiaChampionsStrategy(Item):
+    url = Field()
+    urldoc = Field()
+
+class WikiaChampionsSkinsTrivia(Item):
+    url = Field()
+    urldoc = Field()
